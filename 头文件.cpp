9cd3f832bf0c2c4e265cfa2d16140320ff5e5d21
byjiang98 <446235591@qq.com>
2017-12-05 15:33:33 +0800

    Update 头文件.cpp

--- 头文件.cpp
+++ 头文件.cpp
@@ -1,6 +1,8 @@
 #define rep(i, a, n) for (int i = a; i < n; ++i)
 #define all(x) (x).begin(), (x).end()
 #define mst(a, b) memset(a, b, sizeof(a))
+#define x first
+#define y second
 typedef long long ll;
 typedef pair<int, int> pii;
 const int mod = 1e9 + 7;
