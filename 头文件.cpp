1e53a0ab52f93625810a87c92776c82efdbbae37
byjiang98 <446235591@qq.com>
2017-12-05 15:33:01 +0800

    Update 头文件.cpp

--- 头文件.cpp
+++ 头文件.cpp
@@ -1,1 +1,7 @@
-
+#define rep(i, a, n) for (int i = a; i < n; ++i)
+#define all(x) (x).begin(), (x).end()
+#define mst(a, b) memset(a, b, sizeof(a))
+typedef long long ll;
+typedef pair<int, int> pii;
+const int mod = 1e9 + 7;
+const int inf = 0x3f3f3f3f;
